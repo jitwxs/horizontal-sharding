423721ece1c14434122436ea17684353a141f6a9
jitwxs <jitwxs@foxmail.com>
2021-03-01 00:12:20 +0800

    add bugs waiting fix

--- mybatis-impl/src/test/java/com/github/jitwxs/sharding/horizontal/mybatisimpl/CenterTransactionTest.java
+++ mybatis-impl/src/test/java/com/github/jitwxs/sharding/horizontal/mybatisimpl/CenterTransactionTest.java
@@ -3,9 +3,9 @@ package com.github.jitwxs.sharding.horizontal.mybatisimpl;
 import com.github.jitwxs.sharding.horizontal.common.DateUtils;
 import com.github.jitwxs.sharding.horizontal.mybatisimpl.entiy.User;
 import com.github.jitwxs.sharding.horizontal.mybatisimpl.mapper.center.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author jitwxs
  * @date 2020年02月15日 19:26
  */
+@Slf4j
 public class CenterTransactionTest extends BaseTest {
     @Autowired
     private UserMapper userMapper;
@@ -26,8 +27,8 @@ public class CenterTransactionTest extends BaseTest {
      * @date 2020/2/16 17:42
      */
     @Test
-    @Commit
-    @Transactional(rollbackFor = Exception.class)
+    @Transactional(transactionManager = "centerTransactionManager", rollbackFor = Exception.class)
+    @Commit // junit 下必须要 commit，实际使用不需要
     public void testCommit() {
         long id1 = DateUtils.now(), id2 = id1 + RandomUtils.nextInt();
 
@@ -37,39 +38,6 @@ public class CenterTransactionTest extends BaseTest {
         userMapper.insert(user1);
         userMapper.insert(user2);
 
-        user1 = userMapper.selectByUserId(id1);
-        user2 = userMapper.selectByUserId(id2);
-
-        Assert.assertNotNull(user1);
-        Assert.assertNotNull(user2);
-    }
-
-    /**
-     * 事务回滚
-     * @author jitwxs
-     * @date 2020/2/16 17:42
-     */
-    @Test
-    @Transactional(rollbackFor = Exception.class)
-    public void testRollback() {
-        long id1 = DateUtils.now(), id2 = id1 + RandomUtils.nextInt();
-
-        User user1 = User.builder().id(id1).username(RandomStringUtils.randomAscii(4)).phone(RandomStringUtils.randomNumeric(5)).build();
-        User user2 = User.builder().id(id2).username(RandomStringUtils.randomAscii(4)).phone(RandomStringUtils.randomNumeric(5)).build();
-
-        try {
-            userMapper.insert(user1);
-
-            int i = 1 / 0;
-
-            userMapper.insert(user2);
-        } catch (Exception e) {
-        }
-
-        user1 = userMapper.selectByUserId(id1);
-        user2 = userMapper.selectByUserId(id2);
-
-        Assert.assertNull(user1);
-        Assert.assertNull(user2);
+        log.error("手动查询数据库，是否存在记录：{},{}，期望存在", user1.getId(), user2.getId());
     }
 }
--- mybatis-impl/src/test/java/com/github/jitwxs/sharding/horizontal/mybatisimpl/ShardingTransactionTest.java
+++ mybatis-impl/src/test/java/com/github/jitwxs/sharding/horizontal/mybatisimpl/ShardingTransactionTest.java
@@ -10,8 +10,10 @@ import com.github.jitwxs.sharding.horizontal.mybatisimpl.mapper.shard.OrderMappe
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -27,18 +29,24 @@ public class ShardingTransactionTest extends BaseTest {
     @Autowired
     private OrderDescMapper orderDescMapper;
 
+    public long userId;
+
+    @Before
+    public void createUser() {
+        User user = User.builder().username(RandomStringUtils.randomAscii(4)).phone(RandomStringUtils.randomNumeric(5)).build();
+        userMapper.insert(user);
+        userId = user.getId();
+    }
+
     /**
-     * 事务提交
+     * 事务提交 TODO 有bug，需要指定datasource
      * @author jitwxs
      * @date 2020/2/16 17:42
      */
     @Test
-    @Transactional(rollbackFor = Exception.class)
+    @Transactional(transactionManager = "shardingTransactionManager", rollbackFor = Exception.class)
+    @Commit // junit 下必须要 commit，实际使用不需要
     public void testCommit() {
-        User user = User.builder().username(RandomStringUtils.randomAscii(4)).phone(RandomStringUtils.randomNumeric(5)).build();
-        userMapper.insert(user);
-
-        final long userId = user.getId();
         final int modulo = ShardingContext.getModulo(userId);
 
         orderMapper.removeAll(modulo);
@@ -62,40 +70,5 @@ public class ShardingTransactionTest extends BaseTest {
         orderDesc = orderDescMapper.selectById(orderDescId, modulo);
         Assert.assertNotNull(orderDesc);
         Assert.assertEquals(userId, orderDesc.getUserId());
-    }
-
-    /**
-     * 事务回滚
-     * @author jitwxs
-     * @date 2020/2/16 17:42
-     */
-    @Test
-    @Transactional(rollbackFor = Exception.class)
-    public void testRollback() {
-        User user = User.builder().username(RandomStringUtils.randomAscii(4)).phone(RandomStringUtils.randomNumeric(5)).build();
-        userMapper.insert(user);
-
-        final long userId = user.getId();
-        final int modulo = ShardingContext.getModulo(userId);
-
-        orderMapper.removeAll(modulo);
-        orderDescMapper.removeAll(modulo);
-
-        double amount = RandomUtils.nextDouble(3, 6);
-        Order order = Order.builder().userId(userId).amount(amount).build();
-        orderMapper.insert(order, modulo);
-
-        final long orderId = order.getId();
-
-        OrderDesc orderDesc = OrderDesc.builder().orderId(orderId).userId(userId).description(RandomStringUtils.randomAscii(8)).build();
-        orderDescMapper.insert(orderDesc);
-
-        final long orderDescId = orderDesc.getId();
-
-        order = orderMapper.selectById(orderId, modulo);
-        Assert.assertNull(order);
-
-        orderDesc = orderDescMapper.selectById(orderDescId, modulo);
-        Assert.assertNull(orderDesc);
     }
 }
